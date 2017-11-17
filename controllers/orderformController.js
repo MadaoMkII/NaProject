251ccb62f17defa39bef3736c5f35e555465d370
MadaoMkII <33247241+MadaoMkII@users.noreply.github.com>
2017-11-16 17:40:35 -0800

    save

--- controllers/orderformController.js
+++ controllers/orderformController.js
@@ -1,9 +1,10 @@
 const checkOrderModel = require('../modules/checkOrder');
 const angentModel = require('../modules/agent');
-exports.addOrderForm = function (req, res, next) {
+const orderModel = require('../modules/orderForm');
+exports.addOrderForm = function (req, res) {
 
     let orderInformation = req.body;
-    let flag = false;
+    let flag = false, orderErrors = [];
     let publishPositions = orderInformation.publishPositions;
 
     if (publishPositions.includes(orderInformation.receivePosition)) {
@@ -13,66 +14,75 @@ exports.addOrderForm = function (req, res, next) {
     }
     let tempNumber = publishPositions.length;
 
-    if (null !== publishPositions && Array.isArray(publishPositions)) {
+    new orderModel(orderInformation).save((err) => {
+        if (err) {
+            console.log(orderErrors);
+            orderErrors.push(err);
+        }
+    }).then(() => {
+        if (null !== publishPositions && Array.isArray(publishPositions)) {
+            angentModel.findByPositionName(publishPositions, (error, result) => {
+                if (error) return res.status(503).send({success: false, message: 'Error happen when adding to DB'});
+                result.forEach((element) => {
+                    let shouldPay, checkOrder;
+                    if (flag || element.stationname !== orderInformation.receivePosition) {
+                        shouldPay = Math.round(orderInformation.orderTotalAmont
+                            / tempNumber * element.publishrate * 100) / 100;
 
-        angentModel.findByPositionName(publishPositions, (error, result) => {
-            if (error) return res.status(503).send({success: false, message: 'Error happen when adding to DB'});
-            result.forEach((element) => {
-                let shouldPay, checkOrder;
-                if (flag || element.stationname !== orderInformation.receivePosition) {
-                    shouldPay = Math.round(orderInformation.orderTotalAmont
-                        / tempNumber * element.publishrate * 100) / 100;
+                        checkOrder = {
+                            adStatus: 'Ongoing',
+                            adName: orderInformation.adName,
+                            adBeginDate: orderInformation.adBeginDate,
+                            adEndDate: orderInformation.adEndDate,
+                            receivePosition: orderInformation.receivePosition,
+                            customerWechat: orderInformation.customerWechat,
+                            orderAmont: shouldPay,
+                            remark: orderInformation.remark
+                        };
 
-                    checkOrder = {
-                        adStatus: 'Ongoing',
-                        adName: orderInformation.adName,
-                        adBeginDate: orderInformation.adBeginDate,
-                        adEndDate: orderInformation.adEndDate,
-                        receivePosition: orderInformation.receivePosition,
-                        customerWechat: orderInformation.customerWechat,
-                        orderAmont: shouldPay,
-                        remark: orderInformation.remark
-                    };
+                        new checkOrderModel(checkOrder).save((err) => {
 
-                  let x=  new checkOrderModel(checkOrder).save().catch((err) => {
-                        console.log(111111);
-                        res.status(503).json({success: false, message: 'Error happen when adding to DB'});
-                        return 'a';
-                    });
-                }
-                if (element.stationname === orderInformation.receivePosition) {
-                    let shouldPay = Math.round(orderInformation.orderTotalAmont
-                        * element.receiverate * 100) / 100;
+                            if (err) {
+                                console.log(err);
+                                orderErrors.push(err);
+                            }
 
-                    let checkOrder = {
-                        adStatus: 'Ongoing',
-                        adName: orderInformation.adName,
-                        adBeginDate: orderInformation.adBeginDate,
-                        adEndDate: orderInformation.adEndDate,
-                        receivePosition: orderInformation.receivePosition,
-                        publishPosition: element.stationname,
-                        customerWechat: orderInformation.customerWechat,
-                        orderAmont: shouldPay,
-                        remark: orderInformation.remark
-                    };
+                            if (element.stationname === orderInformation.receivePosition) {
+                                let shouldPay = Math.round(orderInformation.orderTotalAmont
+                                    * element.receiverate * 100) / 100;
 
-                    new checkOrderModel(checkOrder).save((err, data) => {
+                                let checkOrder = {
+                                    adStatus: 'Ongoing',
+                                    adName: orderInformation.adName,
+                                    adBeginDate: orderInformation.adBeginDate,
+                                    adEndDate: orderInformation.adEndDate,
+                                    receivePosition: orderInformation.receivePosition,
+                                    publishPosition: element.stationname,
+                                    customerWechat: orderInformation.customerWechat,
+                                    orderAmont: shouldPay,
+                                    remark: orderInformation.remark
+                                };
 
-                        if (err) {
-                            return res.status(209).send({message: 'hhhh'})
-                        } else {
-                            return res.status(201).send({message: '@!!!!!!!'})
-                            // res.status(200); .json({success: true, message: 'Order has been added to DB'});
-                        }
-
-                    });
-
-                }
-
-            })
-        });
-
-    }
+                                new checkOrderModel(checkOrder).save((err) => {
+                                    if (err) {
+                                        orderErrors.push(err);
+                                    }
+                                    if (orderErrors.length !== 0) {
+                                        return res.status(503).send({
+                                            success: false,
+                                            message: 'Error Happened , please check input data!'
+                                        });
+                                    } else {
+                                        return res.status(200).send({success: true, message: 'Successed Saved'});
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            });
+        }
+    })
 
 
 };
